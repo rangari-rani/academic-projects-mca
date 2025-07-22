c62699b2c0e01d024c5b0bc99e72a36a0b922dab
Rani Rangari <ranirangari2018@gmail.com>
2025-07-22 16:34:20 +0530

    Update App.jsx

--- artisans-collectives/src/App.jsx
+++ artisans-collectives/src/App.jsx
@@ -5,7 +5,6 @@ import Collection from './pages/Collection'
 import About from './pages/About'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
-import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
@@ -28,7 +27,7 @@ const App = () => {
             <Route path="/about" element={<About />} />
             <Route path="/product/:productId" element={<Product />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<Login />} />
+         
             <Route path="/place-order" element={<PlaceOrder />} />
             <Route path="/orders" element={<Orders />} />
             <Route path="/order-success" element={<OrderSuccess />} />
