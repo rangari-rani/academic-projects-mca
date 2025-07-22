d573a254bd29fbc612206c64346d5551417967a9
Rani Rangari <ranirangari2018@gmail.com>
2025-07-22 17:13:03 +0530

    Update App.js

--- dijkstra-path-visualizer/src/App.js
+++ dijkstra-path-visualizer/src/App.js
@@ -6,6 +6,19 @@ function App() {
   return (
     <div className="App">
       <PathfindingVisualizer></PathfindingVisualizer>
+    <footer className="footer">
+  <p>Built by Rani Rangari • MCA Project • 2022</p>
+  <p>
+    <a href="https://github.com/rangari-rani/" target="_blank" rel="noopener noreferrer">
+      GitHub
+    </a>
+    &nbsp;|&nbsp;
+    <a href="https://www.linkedin.com/in/rani-rangari/" target="_blank" rel="noopener noreferrer">
+      LinkedIn
+    </a>
+  </p>
+</footer>
+
     </div>
   );
 }
